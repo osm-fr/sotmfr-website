f2374789de52135de28cd887b65827946a0cbc45
Aline <chetta.aline@gmail.com>
2020-03-01 16:47:00 +0100

    Fix liste de diffusion + Crédits (attribution)

--- 2020/assets/js/navbar-collapse.js
+++ 2020/assets/js/navbar-collapse.js
@@ -28,8 +28,3 @@ $(function() {
         }
     });
 });
-
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
-});
