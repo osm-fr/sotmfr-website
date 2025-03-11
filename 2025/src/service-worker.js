1f6fb4f3ac1f09fe937ca2f98d88f40cc5236ea7
Jean-Baptiste <87148630+Jean-BaptisteC@users.noreply.github.com>
2025-03-11 10:02:36 +0100

    Réimplantation du service worker

---
+++ 2025/src/service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'sotm-2025';
+const urlsToCache = [
+   '/',
+   'index.html',
+   '/css/main.css',
+   '/js/main.js'
+];
+ 
+ // Install event
+ self.addEventListener('install', (event) => {
+   event.waitUntil(
+     caches.open(CACHE_NAME).then((cache) => {
+       return cache.addAll(urlsToCache);
+     })
+   );
+ });
+ 
+ // Fetch event
+ self.addEventListener('fetch', (event) => {
+   event.respondWith(
+     caches.match(event.request).then((response) => {
+       return response || fetch(event.request);
+     })
+   );
+ });
+ 
+ // Activate event
+ self.addEventListener('activate', (event) => {
+   const cacheWhitelist = [CACHE_NAME];
+   event.waitUntil(
+     caches.keys().then((cacheNames) => {
+       return Promise.all(
+         cacheNames.map((cacheName) => {
+           if (!cacheWhitelist.includes(cacheName)) {
+             return caches.delete(cacheName);
+           }
+         })
+       );
+     })
+   );
+ });
