9c7386e2790ebcca9346ee74334a408f7056c4fa
Jean-Baptiste <jeanbaptiste.charron@outlook.fr>
2025-04-18 15:04:39 +0200

    Ajout du script matomo

---
+++ 2025/src/js/matomo.js
@@ -0,0 +1,11 @@
+var _paq = window._paq = window._paq || [];
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+
+    (function() {
+      var u="https://stats.openstreetmap.fr/";
+      _paq.push(['setTrackerUrl', u+'matomo.php']);
+      _paq.push(['setSiteId', '']);
+      var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+      g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+    })();
